175ca7c2575b95aacf659f3a4af8bb8c5ad1806c
Pietro Tomelin Serpa <pietro.tomelin33@gmail.com>
2021-03-26 21:39:34 -0300

    first commit

---
+++ js/script_form.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.coordenadas').mouseenter(function() {
+        $('.valores').show();
+    });
+    $('.coordenadas').mousemove(function(e) {
+        var posicoes = $(this).offset();
+
+        var x = e.clientX - posicoes.left;
+        var y = e.clientY - posicoes.top;
+
+        $('.valores').html(`coordenadas x: ${x} e y: ${y}`);
+    });
+    $('.coordenadas').mouseout(function() {
+        $('.valores').fadeOut();
+    });
+});
